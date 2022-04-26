e2e9b094634f7e85fb94ef1e15b6789f880d2b4d
28apuri <103583844+28apuri@users.noreply.github.com>
2022-04-25 21:44:53 -0700

    Create MadLibs.py

---
+++ MadLibs.py
@@ -0,0 +1,12 @@
+def madlibs():
+  animals= input('Enter a animal name : ')
+  color = input('Enter a color name: ')
+  item = input('Enter a item name: ')
+  name= input('Enter a person name: ')
+  place = input('Enter a place name: ')
+  verb = input('Enter a verb: ')
+  food = input('Enter a food name: ')
+
+  print("There was once a guy named " + name + "He loved to eat " + food + "He had a favorite color of  " + color + "This was a very " + verb + "color" + "  )
+
+madlibs()
